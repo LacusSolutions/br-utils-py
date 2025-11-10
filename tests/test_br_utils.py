de30fd0cd1019028fa95faee71c6a5e7115c2946
juliolmuller <juliolmuller@outlook.com>
2025-11-10 16:33:08 -0300

    refactor: lint source code

--- tests/test_br_utils.py
+++ tests/test_br_utils.py
@@ -15,6 +15,7 @@ def test_dependencies_available():
     """Testa se as dependências estão disponíveis."""
     try:
         from lacus import cnpj_utils, cpf_utils
+
         assert cnpj_utils is not None
         assert cpf_utils is not None
     except ImportError:
