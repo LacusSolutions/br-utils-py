3a4f39b88084006f87f86bc88baa055d59f5f227
juliolmuller <juliolmuller@outlook.com>
2025-11-14 12:18:13 -0300

    feat(cnpj-fmt): create function for easier use

--- packages/cnpj-fmt/src/cnpj_fmt/cnpj_fmt.py
+++ packages/cnpj-fmt/src/cnpj_fmt/cnpj_fmt.py
@@ -2,12 +2,9 @@ from collections.abc import Callable
 
 from .cnpj_formatter import CnpjFormatter
 
-CNPJ_LENGTH = 14
-
 
 def cnpj_fmt(
     cnpj_string: str,
-    escape: bool | None = None,
     hidden: bool | None = None,
     hidden_key: str | None = None,
     hidden_start: int | None = None,
@@ -15,18 +12,19 @@ def cnpj_fmt(
     dot_key: str | None = None,
     slash_key: str | None = None,
     dash_key: str | None = None,
+    escape: bool | None = None,
     on_fail: Callable | None = None,
 ) -> str:
     formatter = CnpjFormatter(
-        escape=escape,
-        hidden=hidden,
-        hidden_key=hidden_key,
-        hidden_start=hidden_start,
-        hidden_end=hidden_end,
-        dot_key=dot_key,
-        slash_key=slash_key,
-        dash_key=dash_key,
-        on_fail=on_fail,
+        hidden,
+        hidden_key,
+        hidden_start,
+        hidden_end,
+        dot_key,
+        slash_key,
+        dash_key,
+        escape,
+        on_fail,
     )
 
     return formatter.format(cnpj_string)
