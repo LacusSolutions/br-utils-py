4b4b7f7c673393ab0136398808e20aed895d6bc9
juliolmuller <juliolmuller@outlook.com>
2025-11-21 13:08:37 -0300

    refactor(cnpj-val): improve code quality for Python context

--- src/cnpj_val/cnpj_validator.py
+++ src/cnpj_val/cnpj_validator.py
@@ -1,43 +1,50 @@
-import re
-
-from cnpj_gen import CnpjGeneratorVerifierDigit
+from cnpj_gen import CnpjGeneratorVerifierDigit as CnpjVerifierDigit
 
 CNPJ_LENGTH = 14
 
 
 class CnpjValidator:
-    def __init__(self):
-        self.verifier_digit = CnpjGeneratorVerifierDigit()
+    __slots__ = ("_verifier_digit",)
+
+    def __init__(self) -> None:
+        self._verifier_digit = CnpjVerifierDigit()
 
     def is_valid(self, cnpj_string: str) -> bool:
-        cnpj_numbers_string = re.sub(r"[^0-9]", "", cnpj_string)
-        cnpj_numbers_array = [int(digit) for digit in cnpj_numbers_string]
+        cnpj_numbers_string = "".join(filter(str.isdigit, cnpj_string))
 
-        if len(cnpj_numbers_array) != CNPJ_LENGTH:
+        if len(cnpj_numbers_string) != CNPJ_LENGTH:
             return False
 
+        cnpj_numbers_array = [int(digit) for digit in cnpj_numbers_string]
+
+        if not self._validate_first_verifier_digit(cnpj_numbers_array):
+            return False
+
+        if not self._validate_second_verifier_digit(cnpj_numbers_array):
+            return False
+
+        return True
+
+    def _validate_first_verifier_digit(self, cnpj_numbers_array: list[int]) -> bool:
         first_verifier_digit_index = CNPJ_LENGTH - 2
         provided_first_verifier_digit = cnpj_numbers_array[first_verifier_digit_index]
         base_first_verifier_digit_calculation = cnpj_numbers_array[
             :first_verifier_digit_index
         ]
-        calculated_first_verifier_digit = self.verifier_digit.calculate(
+        calculated_first_verifier_digit = self._verifier_digit.calculate(
             base_first_verifier_digit_calculation
         )
 
-        if provided_first_verifier_digit != calculated_first_verifier_digit:
-            return False
+        return provided_first_verifier_digit == calculated_first_verifier_digit
 
+    def _validate_second_verifier_digit(self, cnpj_numbers_array: list[int]) -> bool:
         second_verifier_digit_index = CNPJ_LENGTH - 1
+        provided_second_verifier_digit = cnpj_numbers_array[second_verifier_digit_index]
         base_second_verifier_digit_calculation = cnpj_numbers_array[
             :second_verifier_digit_index
         ]
-        provided_second_verifier_digit = cnpj_numbers_array[second_verifier_digit_index]
-        calculated_second_verifier_digit = self.verifier_digit.calculate(
+        calculated_second_verifier_digit = self._verifier_digit.calculate(
             base_second_verifier_digit_calculation
         )
 
-        if provided_second_verifier_digit != calculated_second_verifier_digit:
-            return False
-
-        return True
+        return provided_second_verifier_digit == calculated_second_verifier_digit
