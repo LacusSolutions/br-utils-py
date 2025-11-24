91f6b5f44352f6188e553c905a30ed9f11c28484
juliolmuller <juliolmuller@outlook.com>
2025-11-24 16:42:35 -0300

    feat(cnpj-cd): create class to calculate check digits in CNPJ

--- src/cnpj_cd/__init__.py
+++ src/cnpj_cd/__init__.py
@@ -1,1 +1,15 @@
-from .cnpj_cd import cnpj_cd
+from .cnpj_check_digits import CnpjCheckDigits
+from .exceptions import (
+    CnpjCheckDigitsCalculationError,
+    CnpjCheckDigitsError,
+    CnpjInvalidLengthError,
+    CnpjTypeError,
+)
+
+__all__ = [
+    "CnpjCheckDigits",
+    "CnpjCheckDigitsCalculationError",
+    "CnpjCheckDigitsError",
+    "CnpjInvalidLengthError",
+    "CnpjTypeError",
+]
--- src/cnpj_cd/cnpj_cd.py
+++
@@ -1,5 +0,0 @@
-__all__ = ["cnpj_cd"]
-
-
-def cnpj_cd():
-    return "Hello, 'cnpj-cd'!"
