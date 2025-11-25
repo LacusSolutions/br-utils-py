ed21525ad877d9e32ccc161f67c60d3917f12ede
juliolmuller <juliolmuller@outlook.com>
2025-11-25 12:07:09 -0300

    docs(cnpj-val): add resources docstrings

--- packages/cnpj-val/src/cnpj_val/cnpj_val.py
+++ packages/cnpj-val/src/cnpj_val/cnpj_val.py
@@ -1,7 +1,6 @@
 from .cnpj_validator import CnpjValidator
 
-__all__ = ["cnpj_val"]
-
 
 def cnpj_val(cnpj_string: str) -> bool:
+    """Validates a CNPJ string."""
     return CnpjValidator().is_valid(cnpj_string)
