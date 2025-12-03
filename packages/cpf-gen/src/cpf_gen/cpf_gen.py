e31e33f956bf0eecc0992888dc5dad22e5b65a9e
juliolmuller <juliolmuller@outlook.com>
2025-12-03 17:43:54 -0300

    feat(cpf-gen): implement package features

--- packages/cpf-gen/src/cpf_gen/cpf_gen.py
+++ packages/cpf-gen/src/cpf_gen/cpf_gen.py
@@ -1,5 +1,8 @@
-__all__ = ["cpf_gen"]
+from .cpf_generator import CpfGenerator
 
 
-def cpf_gen():
-    return "Hello, 'cpf-gen'!"
+def cpf_gen(format: bool | None = None, prefix: str | None = None) -> str:
+    """Generate a valid CPF according to the given options."""
+    generator = CpfGenerator(format, prefix)
+
+    return generator.generate()
